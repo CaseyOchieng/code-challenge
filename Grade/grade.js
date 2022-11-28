2e3a318a43e2b6529bd61489a8ca1cbaa00ca441
Casey Ochieng <caseyochieng46@gmail.com>
2022-11-28 09:11:20 +0300

    completed assignment

---
+++ Grade/grade.js
@@ -0,0 +1,22 @@
+
+let score = 100;
+let grade;
+if(score >=0 && score <= 100){
+if(score >= 90){
+    grade ='A'
+    console.log(grade);
+}
+else if(score >= 80){
+    grade ='B'
+}
+else if(score => 70){
+    grade = 'C'
+}
+else if(score >= 60){
+    grade ='D'
+}
+else(score <40){
+    grade ='E'
+}
+
+
