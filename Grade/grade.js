ac628089a52a67e5b28335ce9f095aba1ef1ea34
Casey Ochieng <caseyochieng46@gmail.com>
2022-11-28 12:57:45 +0300

    completed code challenge

--- Grade/grade.js
+++ Grade/grade.js
@@ -19,6 +19,34 @@
 //         grade ='E'
 //     }
 //     print('score:',score ,"and your Grade is"&{grade}) 
+function studentGrading(){
+let score =prompt("Enter score")
+let grade;
+// const msg= function(grade){
+    // return `you Grade is'{grade}`
+// }
+if(score >=0 && score <= 100){
+    if(score >= 79){
+        grade ='A'
+    }
+    else if(score >59 && score <79){
+        grade ='B'
+    }
+    else if(score >39 && score <49){
+        grade ='C'
+    }
+    else if(score <40){
+        grade ='D'
+    }
+    else{
+        grade ='E'
+    }
     
-    
+document.body.innerHTML=`you Grade is ${grade}`
+}
+else {
+    document.body.innerHTML="invalid Score"
+}
+}
+studentGrading()
 
